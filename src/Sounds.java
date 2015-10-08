a4aa00e9cebdf7fc2a2b56206d99f098d97f2ff2
evan4james <evan4james@yahoo.com>
2015-10-07 22:09:02 -0700

    added background music :D

--- src/Sounds.java
+++ src/Sounds.java
@@ -1,4 +1,4 @@
-//test
+
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -9,29 +9,29 @@ public class Sounds {
    private Clip clip;
 
    public Sounds(String fileName) {
- 
+
       try {
-         
+
          AudioInputStream sound = AudioSystem.getAudioInputStream(getClass().getResource(fileName));
          clip = AudioSystem.getClip();
          clip.open(sound);
-         
+
       } catch (MalformedURLException e) {
          e.printStackTrace();
          throw new RuntimeException("Sound: Malformed URL: " + e);
-         
+
       } catch (UnsupportedAudioFileException e) {
          e.printStackTrace();
          throw new RuntimeException("Sound: Unsupported Audio File: " + e);
-         
+
       } catch (IOException e) {
          e.printStackTrace();
          throw new RuntimeException("Sound: Input/Output Error: " + e);
-         
+
       } catch (LineUnavailableException e) {
          e.printStackTrace();
          throw new RuntimeException("Sound: Line Unavailable Exception Error: " + e);
-         
+
       }
    }
 
