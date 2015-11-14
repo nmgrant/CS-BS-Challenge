a90eaec468692b1a7dc8198c3117470693e83d86
Nicholas Grant <ngrant40@gmail.com>
2015-11-13 23:24:15 -0800

    Completed reward choice functionality

--- src/CardClasses/RewardChoiceDialog.java
+++ src/CardClasses/RewardChoiceDialog.java
@@ -3,33 +3,40 @@ package CardClasses;
 import javax.swing.JDialog;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import GameClasses.SkillPoints;
+import java.awt.BorderLayout;
+import javax.swing.SwingUtilities;
+import sun.awt.AWTAccessor;
 
 /**
  *
  * @author Nicholas Grant
  */
 public class RewardChoiceDialog extends JDialog {
-    
+
     private final int LEARNING_BUTTON = 0;
     private final int CRAFT_BUTTON = 1;
     private final int INTEGRITY_BUTTON = 2;
-    
+
     private JButton learningButton;
     private JButton craftButton;
     private JButton integrityButton;
-    
-    private final JButton[] buttons = 
-     {learningButton, craftButton, integrityButton};
-    
+
+    private JButton[] buttons;
+
     private JLabel chooseReward;
-    
+
+    private SkillPoints choice;
+
     public RewardChoiceDialog(SkillPoints[] skillRewardChoices) {
+        setTitle("Reward Choice");
         initializeComponents();
         enableButtons(skillRewardChoices);
 
-//<editor-fold defaultstate="collapsed" desc="Set dialog layout">
+        //<editor-fold defaultstate="collapsed" desc="Set dialog layout">
         javax.swing.GroupLayout rewardChoiceDialogLayout = new javax.swing.GroupLayout(this.getContentPane());
         this.getContentPane().setLayout(rewardChoiceDialogLayout);
         rewardChoiceDialogLayout.setHorizontalGroup(
@@ -41,9 +48,9 @@ public class RewardChoiceDialog extends JDialog {
                                 .addGroup(rewardChoiceDialogLayout.createSequentialGroup()
                                         .addComponent(learningButton)
                                         .addGap(18, 18, 18)
-                                        .addComponent(integrityButton, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addComponent(craftButton)))
                         .addGap(18, 18, 18)
-                        .addComponent(craftButton)
+                        .addComponent(integrityButton)
                         .addContainerGap(13, Short.MAX_VALUE))
         );
         rewardChoiceDialogLayout.setVerticalGroup(
@@ -58,62 +65,76 @@ public class RewardChoiceDialog extends JDialog {
                                 .addComponent(craftButton, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-//</editor-fold>
+        //</editor-fold>
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        
-        this.setModal(true);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
+
+        setModal(true);
+        pack();
+        setVisible(true);
     }
-    
+
     private void enableButtons(SkillPoints[] skillRewardChoices) {
         for (int i = 0; i < skillRewardChoices.length; i++) {
-            if (skillRewardChoices[i] == new SkillPoints(i)) {
+            if (skillRewardChoices[i].equals(new SkillPoints(i))) {
                 buttons[i].setEnabled(true);
             }
         }
     }
+
     private void initializeComponents() {
         learningButton = new JButton();
         craftButton = new JButton();
         integrityButton = new JButton();
         chooseReward = new JLabel();
-        
-        learningButton.setText("Learning Chip");
+
+        buttons = new JButton[3];
+        buttons[LEARNING_BUTTON] = learningButton;
+        buttons[CRAFT_BUTTON] = craftButton;
+        buttons[INTEGRITY_BUTTON] = integrityButton;
+
+        learningButton.setText("Learning");
         learningButton.addActionListener(new java.awt.event.ActionListener() {
+            @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                learningButtonActionPerformed(evt);
+                choice = new SkillPoints(LEARNING_BUTTON);
+                setVisible(false);
+                dispose();
             }
         });
-        
-        craftButton.setText("Integrity Chip");
+
+        craftButton.setText("Craft");
         craftButton.addActionListener(new java.awt.event.ActionListener() {
+            @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                craftButtonActionPerformed(evt);
+                choice = new SkillPoints(CRAFT_BUTTON);
+                setVisible(false);
+                dispose();
             }
         });
-        
-        integrityButton.setText("Craft Chip");
+
+        integrityButton.setText("Integrity");
         integrityButton.addActionListener(new java.awt.event.ActionListener() {
+            @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                integrityButtonActionPerformed(evt);
+                choice = new SkillPoints(INTEGRITY_BUTTON);
+                setVisible(false);
+                dispose();
             }
         });
         chooseReward.setText("Choose your reward");
-        
+
         learningButton.setEnabled(false);
         craftButton.setEnabled(false);
         integrityButton.setEnabled(false);
+
+        getContentPane().add(learningButton);
+        getContentPane().add(craftButton);
+        getContentPane().add(integrityButton);
+        getContentPane().add(chooseReward);
     }
-    
-    private SkillPoints learningButtonActionPerformed(ActionEvent evt) {
-        return new SkillPoints(1, 0, 0);
-    }
-    
-    private SkillPoints craftButtonActionPerformed(ActionEvent evt) {
-        return new SkillPoints(0, 1, 0);
-    }
-    
-    private SkillPoints integrityButtonActionPerformed(ActionEvent evt) {
-        return new SkillPoints(0, 0, 1);
+
+    public SkillPoints getChoice() {
+        return choice;
     }
 }
